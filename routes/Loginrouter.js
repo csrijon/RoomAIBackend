abf2d003b24b96a3639174a6f9422832009214dd
Srijon Chowdhury <csrijon92@gmail.com>
2026-01-10 18:03:49 +0530

    push

--- routes/Loginrouter.js
+++ routes/Loginrouter.js
@@ -1,12 +1,19 @@
 import express from "express";
 const router = express.Router();
+import { signupModel } from "../models/Schema.js";
 
-router.post("/",(req,res)=>{
+router.post("/", async (req, res) => {
     const { usermail, loginpass } = req.body;
     console.log("Login attempt:", usermail, loginpass);
-    res.send("login route working")
-})
 
+    const user = await signupModel.findOne({ usermail, signuppass: loginpass });
+    console.log("User found:", user);
+    if (!user) {
+        return res.status(401).json({ success: false, message: "Invalid email or password" });
+    }
+
+    res.status(200).json({ success: true, message: "login route working" });
+})
 
 
 export default router;
