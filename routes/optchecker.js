d117df754607ee1c16706e2be203745a5b92ea55
Srijon Chowdhury <csrijon92@gmail.com>
2026-01-16 00:20:12 +0530

    otp generate

--- routes/optchecker.js
+++ routes/optchecker.js
@@ -2,8 +2,16 @@ import express from 'express';
 const router = express.Router();
 import { signupModel } from '../models/Schema.js';
 
+
+let regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 router.post('/', async (req, res) => {
     const { usermail } = req.body;
+    if (regexEmail.test(usermail)) {
+        console.log('Valid email format');
+    } else {
+        return res.status(400).json({ success: false, message: 'Invalid email format' });
+    }
     console.log('OTP check attempt for:', usermail);
 
     const user = await signupModel.findOne({ usermail });
@@ -11,9 +19,14 @@ router.post('/', async (req, res) => {
     if (!user) {
         return res.status(404).json({ success: false, message: 'Email not registered' });
     }
-
-    res.status(200).json({ success: true, message: 'OTP can be sent to this email' });
+    let otp = genotp();
+    console.log("Generated OTP for user:", otp);
+    res.status(200).json({ success: true, message: 'OTP can be sent to this email', usermail,otp });
 }
-
 );
+
+const genotp = () => {
+    let otp = Math.floor(10000 + Math.random() * 90000);
+    return otp;
+}
 export default router;
